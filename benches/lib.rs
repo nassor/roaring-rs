c3e282806f9e1a6124605e3e29d4016fdaf76503
Nassor Paulino da Silva <nassor@gmail.com>
2019-11-24 11:27:29 -0800

    using bencher crate

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,20 +1,17 @@
-#![feature(test)]
-
-extern crate test;
+#[macro_use]
+extern crate bencher;
 extern crate roaring;
 
-use test::Bencher;
 
+use bencher::{Bencher, black_box};
 use roaring::RoaringBitmap;
 
-#[bench]
 fn create(b: &mut Bencher) {
     b.iter(|| {
         RoaringBitmap::new();
     })
 }
 
-#[bench]
 fn insert1(b: &mut Bencher) {
     b.iter(|| {
         let mut bitmap = RoaringBitmap::new();
@@ -23,7 +20,6 @@ fn insert1(b: &mut Bencher) {
     })
 }
 
-#[bench]
 fn insert2(b: &mut Bencher) {
     b.iter(|| {
         let mut bitmap = RoaringBitmap::new();
@@ -33,41 +29,39 @@ fn insert2(b: &mut Bencher) {
     })
 }
 
-#[bench]
 fn is_subset(b: &mut Bencher) {
     let bitmap: RoaringBitmap = (1..250).collect();
-    b.iter(|| test::black_box(bitmap.is_subset(&bitmap)))
+    b.iter(|| black_box(bitmap.is_subset(&bitmap)))
 }
 
-#[bench]
 fn is_subset_2(b: &mut Bencher) {
     let sub: RoaringBitmap = (1000..8196).collect();
     let sup: RoaringBitmap = (0..16384).collect();
-    b.iter(|| test::black_box(sub.is_subset(&sup)))
+    b.iter(|| black_box(sub.is_subset(&sup)))
 }
 
-#[bench]
 fn is_subset_3(b: &mut Bencher) {
     let sub: RoaringBitmap = (1000..4096).map(|x| x * 2).collect();
     let sup: RoaringBitmap = (0..16384).collect();
-    b.iter(|| test::black_box(sub.is_subset(&sup)))
+    b.iter(|| black_box(sub.is_subset(&sup)))
 }
 
-#[bench]
 fn is_subset_4(b: &mut Bencher) {
     let sub: RoaringBitmap = (0..17).map(|x| 1 << x).collect();
     let sup: RoaringBitmap = (0..65536).collect();
-    b.iter(|| test::black_box(sub.is_subset(&sup)))
+    b.iter(|| black_box(sub.is_subset(&sup)))
 }
 
-#[bench]
 fn remove_range_bitmap(b: &mut Bencher) {
     let mut sub: RoaringBitmap = (0..65536).collect();
     b.iter(|| {
         // carefully delete part of the bitmap
         // only the first iteration will actually change something
         // but the runtime remains identical afterwards
-        test::black_box(sub.remove_range(4096 + 1..65536));
+        black_box(sub.remove_range(4096 + 1..65536));
         assert_eq!(sub.len(), 4096 + 1);
     });
 }
+
+benchmark_group!(benches, create, insert1, insert2, is_subset, is_subset_2, is_subset_3, is_subset_4, remove_range_bitmap);
+benchmark_main!(benches);
